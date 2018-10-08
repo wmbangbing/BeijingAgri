c32dcee88bbb121186b6063c07e2a419b0a18642
MingKan <wmbbkm@163.com>
2018-10-08 09:43:28 +0800

    动态表格

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -1,78 +1,46 @@
 <template>
   <div class="app-container">
-    <el-table
-      v-loading="listLoading"
-      :data="list"
-      element-loading-text="Loading"
-      border
-      fit
-      highlight-current-row>
-      <el-table-column align="center" label="ID" width="95">
-        <template slot-scope="scope">
-          {{ scope.$index }}
-        </template>
-      </el-table-column>
-      <el-table-column label="Title">
-        <template slot-scope="scope">
-          {{ scope.row.title }}
-        </template>
-      </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
-        <template slot-scope="scope">
-          <span>{{ scope.row.author }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
-        <template slot-scope="scope">
-          {{ scope.row.pageviews }}
-        </template>
-      </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
-        <template slot-scope="scope">
-          <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
-        </template>
-      </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
-        <template slot-scope="scope">
-          <i class="el-icon-time"/>
-          <span>{{ scope.row.display_time }}</span>
-        </template>
-      </el-table-column>
-    </el-table>
+    <div>
+      <el-table
+        :data="tableData"
+        style="width: 100%"
+        size="mini"
+        class="table">
+         <el-table-column :label="key" v-for ="(value,key) in tableData[0]"  :key="key" >
+            <template slot-scope="scope">
+                    <span>{{scope.row[key]}}</span>
+            </template>
+        </el-table-column>
+      </el-table>
+      </div>
   </div>
 </template>
 
 <script>
-import { getList } from '@/api/table'
+import axios from "axios"
 
 export default {
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
-      }
-      return statusMap[status]
-    }
-  },
   data() {
     return {
-      list: null,
-      listLoading: true
+      tableData: ""
     }
   },
-  created() {
-    this.fetchData()
-  },
-  methods: {
-    fetchData() {
-      this.listLoading = true
-      getList(this.listQuery).then(response => {
-        this.list = response.data.items
-        this.listLoading = false
-      })
-    }
+  mounted(){
+    var _this = this;
+    axios.get('http://202.114.148.160/webapi/api/values')
+    .then(function (response) {    
+      _this.tableData = response.data;     
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 }
 </script>
+
+<style>
+.table{
+  position: relative
+}
+</style>
+
